9a97ffca03c28ca0a617d5f36d2caaaddf08df5c
shj <josh2kv@gmail.com>
2022-08-20 23:47:39 +0900

    done with tree

--- tree/index.js
+++ tree/index.js
@@ -10,8 +10,52 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  // parent nodes를 arr에 저장
+  // arr의 앞에서부터 parent node를 하나씩 뺌(node에 저장)
+  // children이 있으면 하나씩 빼서 arr의 뒤에 붙임
+  // 그리고 그 parent는 fn에 넘김
+  traverseBF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  // children을 arr의 뒤에 붙이는 대신 앞에 붙임. 나머지 같음
+  traverseDF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
