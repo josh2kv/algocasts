7fdb9e9670864688491b30b81d57d450f2538f94
shj <josh2kv@gmail.com>
2022-10-22 01:25:28 +0900

    solve pyramid 1

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,67 +14,8 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n, row = 0, level = '') {
-  /**
-   * mine
-   */
-  for (let i = 1; i <= n; i++) {
-    const numPound = 2 * i - 1;
-    const numWhiteSpace = n - i;
-
-    let pound = '';
-    let whiteSpace = '';
-
-    for (let j = 1; j <= numPound; j++) {
-      pound += '#';
-    }
-
-    for (let j = 1; j <= numWhiteSpace; j++) {
-      whiteSpace += ' ';
-    }
-
-    console.log(`${whiteSpace}${pound}${whiteSpace}`);
-  }
+function pyramid(n, row = 0, stair = '') {
+  // 1 (2022-10-22)
 }
 
 module.exports = pyramid;
-
-// function pyramid(n) {
-//   const length = 2 * n - 1;
-//   const midPoint = Math.floor(length / 2);
-
-//   for (let row = 0; row < n; row++) {
-//     let level = '';
-
-//     for (let col = 0; col < length; col++) {
-//       if (col >= midPoint - row && col <= midPoint + row) {
-//         level += '#';
-//       } else {
-//         level += ' ';
-//       }
-//     }
-
-//     console.log(level);
-//   }
-// }
-
-// function pyramid(n, row = 0, level = '') {
-//   const length = 2 * n - 1;
-//   const midpoint = Math.floor(length / 2);
-
-//   if (row === n) return;
-
-//   if (level.length === length) {
-//     console.log(level);
-
-//     return pyramid(n, row + 1);
-//   }
-
-//   if (level.length >= midpoint - row && level.length <= midpoint + row) {
-//     level += '#';
-//   } else {
-//     level += ' ';
-//   }
-
-//   pyramid(n, row, level);
-// }
