6f26a706b035bb9d98a5541d70827e07b13b888e
shj <josh2kv@gmail.com>
2022-10-22 00:48:35 +0900

    solve steps 1

--- steps/index.js
+++ steps/index.js
@@ -17,49 +17,8 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  /**
-   * mine
-   */
-  let gauge = [];
-
-  for (let i = 0; i < n; i++) {
-    gauge.push(' ');
-  }
-
-  for (let i = 0; i < n; i++) {
-    gauge[i] = '#';
-    console.log(gauge.join(''));
-  }
+function steps(n, row = 0, stair = '') {
+  // 1 (2022-10-22)
 }
 
 module.exports = steps;
-
-// function steps(n) {
-//   for (let row = 0; row < n; row++) {
-//     let stair = '';
-
-//     for (let column = 0; column < n; column++) {
-//       if (column <= row) {
-//         stair += '#';
-//       } else {
-//         stair += ' ';
-//       }
-//     }
-
-//     console.log(stair);
-//   }
-// }
-
-// function steps(n, row = 0, stair = '') {
-//   if (n === row) return;
-
-//   if (n === stair.length) {
-//     console.log(stair);
-//     return steps(n, row + 1);
-//   }
-
-//   const add = stair.length <= row ? '#' : ' ';
-
-//   steps(n, row, stair + add);
-// }
