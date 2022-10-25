89593fbb1082d3c802a2e2bc9ba91909f1b4f1ae
shj <josh2kv@gmail.com>
2022-10-25 22:38:06 +0900

    solve fib 1

--- fib/index.js
+++ fib/index.js
@@ -8,66 +8,8 @@
 // Example:
 //   fib(4) === 3
 
-// function memoize(fn) {
-//   const cache = {};
-
-//   function retrieveOrCalculate(...args) {
-//     if (cache[args]) return cache[args];
-
-//     // this로 retrieveOrCalculate의 this를, arguments로 retrieveOrCalculate의 args를 줘서 slowFib를 호출한다.
-//     const result = fn.apply(this, args);
-//     cache[args] = result;
-
-//     return result;
-//   }
-
-//   return retrieveOrCalculate;
-// }
-
-// function slowFib(n) {
-//   if (n < 2) return n;
-
-//   return fib(n - 1) + fib(n - 2);
-// }
-
-// const fib = memoize(slowFib);
-
-// fib(6);
-
-function* fibonacciSequence() {
-  let x = 0,
-    y = 1;
-  for (;;) {
-    yield y;
-    [x, y] = [y, x + y]; // Note: destructuring assignment
-  }
-}
-
 function fib(n) {
-  for (let f of fibonacciSequence()) {
-    if (n-- <= 1) return f;
-  }
+  // 1 (2022-10-23)
 }
-
+console.log(fib(5));
 module.exports = fib;
-
-// // O(n^2)
-// function fib(n) {
-//   if (n < 2) return n;
-
-//   return fib(n - 1) + fib(n - 2);
-// }
-
-// // O(n)
-// function fib(n) {
-//   const result = [0, 1];
-
-//   for (let i = 2; i <= n; i++) {
-//     const a = result[i - 1];
-//     const b = result[i - 2];
-
-//     result.push(a + b);
-//   }
-
-//   return result[n];
-// }
