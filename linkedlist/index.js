4b9d584d568ce4562d651ebd1b40aaafa91c2490
shj <josh2kv@gmail.com>
2023-04-30 23:06:28 +0900

    Review 16

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -3,91 +3,32 @@
 // See 'directions' document
 
 class Node {
-  constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
-  }
+  constructor(data, next) {}
 }
 
 // 1 (2023-03-25)
 // 2 (2023-03-28)
+// 3 (2023-04-30)
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+  constructor() {}
 
-  insertFirst(data) {
-    this.head = new Node(data, this.head);
-  }
+  insertFirst(data) {}
 
-  size() {
-    let node = this.head;
-    let counter = 0;
-    while (node) {
-      counter++;
-      node = node.next;
-    }
-
-    return counter;
-  }
+  size() {}
 
   // return Node instance not node.data
-  getFirst() {
-    return this.head;
-  }
+  getFirst() {}
 
-  //! hove to return Node lastly not Node.next
-  getLast() {
-    if (!this.head) return null;
+  //! have to return Node lastly not Node.next
+  getLast() {}
 
-    let curNode = this.head;
+  clear() {}
 
-    while (curNode.next) {
-      curNode = curNode.next;
-    }
+  removeFirst() {}
 
-    return curNode;
-  }
+  removeLast() {}
 
-  clear() {
-    this.head = null;
-  }
-
-  removeFirst() {
-    if (!this.head) return;
-
-    this.head = this.head.next;
-  }
-
-  removeLast() {
-    if (!this.head) return;
-
-    if (!this.head.next) {
-      this.head = null;
-      return;
-    }
-
-    let curNode = this.head.next;
-    let preNode = this.head;
-
-    while (curNode.next) {
-      preNode = curNode;
-      curNode = curNode.next;
-    }
-
-    preNode.next = null;
-  }
-
-  insertLast(data) {
-    if (!this.head) {
-      this.head = new Node(data, null);
-      return;
-    }
-
-    const lastNode = this.getLast();
-
-    lastNode.next = new Node(data, null);
-  }
+  insertLast(data) {}
 
   //* index 0 = this.head
   // AGAIN
