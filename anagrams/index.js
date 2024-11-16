6b43d51cf6bb0495424a4d748182f07a405149b2
shj <josh2kv@gmail.com>
2024-11-16 14:39:12 +0900

    Review 7

--- anagrams/index.js
+++ anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -11,6 +11,7 @@
 function anagrams(stringA, stringB) {
   // 2 (2022-10-26)
   // 3 (2023-03-14)
+  // 4 (2024-11-16)
 }
 
 module.exports = anagrams;
